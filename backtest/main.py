6b24ba9c5de34f53b85828aa117aa2dc87526fcc
KOSUKE TERAI <mrtkou0410@gmail.com>
2025-04-10 14:07:12 +0900

    変更したよ

--- backtest/main.py
+++ backtest/main.py
@@ -7,7 +7,7 @@ import shutil
 import json
 import requests
 import uvicorn
-from openai import OpenAI  # OpenAIライブラリのimportだぜ
+from openai import OpenAI  # OpenAIライブラリのimportだぜ!!!!!!!!
 
 load_dotenv()
 
